f72ec3206a352d01e54b680f015686701dc8f5b5
Bobby Sudekum <bobby@mapbox.com>
2017-09-15 09:39:25 -0700

    Initial Commit

---
+++ MapboxVoice.swift/MapboxVoice.swift.h
@@ -0,0 +1,19 @@
+//
+//  MapboxVoice.swift.h
+//  MapboxVoice.swift
+//
+//  Created by Bobby Sudekum on 9/15/17.
+//  Copyright © 2017 Mapbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapboxVoice.swift.
+FOUNDATION_EXPORT double MapboxVoice_swiftVersionNumber;
+
+//! Project version string for MapboxVoice.swift.
+FOUNDATION_EXPORT const unsigned char MapboxVoice_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapboxVoice_swift/PublicHeader.h>
+
+
