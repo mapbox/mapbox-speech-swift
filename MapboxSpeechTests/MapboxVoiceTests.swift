13320be45dc7b1cb466428046fe5160fe2a0b646
Bobby Sudekum <bobby@mapbox.com>
2017-12-07 13:55:52 -0800

    improve tests

--- MapboxSpeechTests/MapboxVoiceTests.swift
+++ MapboxSpeechTests/MapboxVoiceTests.swift
@@ -1,7 +1,8 @@
 import XCTest
 import OHHTTPStubs
-
 @testable import MapboxSpeech
+
+let BogusToken = "pk.foo-bar"
 
 class MapboxVoicZTests: XCTestCase {
     
@@ -11,20 +12,47 @@ class MapboxVoicZTests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        OHHTTPStubs.removeAllStubs()
         super.tearDown()
     }
     
     func testExample() {
-        let voice = SpeechSynthesizer(accessToken: "pk.foo", host: "api.mapbox.com")
-        let options = SpeechOptions(text: "foo")
+        let expectation = self.expectation(description: "Fetching speech audio should return results")
+        
+        let queryParams: [String: String?] = [
+            "textType": "text",
+            "voiceId": "Joanna",
+            "outputFormat": "mp3",
+            "access_token": BogusToken,
+            ]
+        
+        stub(condition: isHost("api.mapbox.com")
+            && isPath("/voice/v1/speak/hello")
+            && containsQueryParams(queryParams)) { _ in
+                let path = Bundle(for: type(of: self)).path(forResource: "hello", ofType: "mp3")
+                return OHHTTPStubsResponse(fileAtPath: path!, statusCode: 200, headers: nil)
+        }
+        
+        let voice = SpeechSynthesizer(accessToken: BogusToken	)
+        let options = SpeechOptions(text: "hello")
         options.textType = .text
         options.voiceId = .joanna
         options.outputFormat = .mp3
         
-        voice.audioData(with: options) { (data: Data?, error: NSError?) in
+        var audio: Data?
+        let task = voice.audioData(with: options) { (data: Data?, error: NSError?) in
             XCTAssertNil(error)
             XCTAssertNotNil(data)
+            audio = data!
+            expectation.fulfill()
         }
+        XCTAssertNotNil(task)
+        
+        waitForExpectations(timeout: 2) { (error) in
+            XCTAssertNil(error, "Error: \(error!)")
+            XCTAssertEqual(task.state, .completed)
+        }
+        
+        XCTAssertNotNil(audio)
     }
 }
