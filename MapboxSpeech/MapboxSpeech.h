95d223f54c4baf4834dfa2764f552d89052a4b80
Bobby Sudekum <bobby@mapbox.com>
2018-01-16 13:19:24 -0800

    rm

--- MapboxSpeech/MapboxSpeech.h
+++ MapboxSpeech/MapboxSpeech.h
@@ -5,5 +5,3 @@ FOUNDATION_EXPORT double MapboxSpeechVersionNumber;
 
 //! Project version string for MapboxSpeech.
 FOUNDATION_EXPORT const unsigned char MapboxSpeechVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MapboxSpeech/PublicHeader.h>
